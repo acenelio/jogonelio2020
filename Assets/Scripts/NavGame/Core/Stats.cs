0bea71fe9c3a32ceb37cddb6c7f102fc5ce14675
Nelio Alves <acenelio@gmail.com>
2020-03-04 17:12:02 -0300

    TouchableGameObject, Stats

---
+++ Assets/Scripts/NavGame/Core/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NavGame.Core
+{
+    [Serializable]
+    public class Stats
+    {
+        public int maxHealth = 100;
+        public int damage = 10;
+        public int armor = 1;
+        public float attackSpeed = 1f;
+    }
+}
---
+++ Assets/Scripts/NavGame/Core/TouchableGameObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core
+{
+    public class TouchableGameObject : MonoBehaviour
+    {
+        public float contactRadius = 0.5f;
+
+        public bool IsInTouch(TouchableGameObject other)
+        {
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            return distance < contactRadius + other.contactRadius;
+        }
+
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, contactRadius);
+        }
+    }
+}
