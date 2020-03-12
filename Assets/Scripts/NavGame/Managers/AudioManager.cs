cebff0c3bd5536848bbf43a5109dca50de466b74
Nelio Alves <acenelio@gmail.com>
2020-03-11 23:19:47 -0300

    AudioManager clips

---
+++ Assets/Scripts/NavGame/Managers/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Managers
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public Clip[] clips;
+
+
+
+        [Serializable]
+        public class Clip
+        {
+            public string name;
+            public AudioClip audio;
+        }
+    }
+}
