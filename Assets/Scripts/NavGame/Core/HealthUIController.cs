55386d7e5ba72648813a6b1066404ecb7b46b46b
Nelio Alves <acenelio@gmail.com>
2020-03-16 10:47:52 -0300

    Health UI instantiation

---
+++ Assets/Scripts/NavGame/Core/HealthUIController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core
+{
+    [RequireComponent(typeof(DamageableGameObject))]
+    public class HealthUIController : MonoBehaviour
+    {
+        public GameObject healthUIPrefab;
+        public Transform healthPosition;
+
+        GameObject healthUI;
+        Transform cam;
+
+        void Awake()
+        {
+            Canvas canvas = FindWorldCanvas();
+            if (canvas == null)
+            {
+                throw new Exception("A WorldSpace canvas was needed");
+            }
+            cam = Camera.main.transform;
+            healthUI = Instantiate(healthUIPrefab, canvas.transform);
+        }
+
+        void LateUpdate()
+        {
+            if (healthUI != null)
+            {
+                healthUI.transform.position = healthPosition.position;
+                healthUI.transform.forward = -cam.forward; 
+            }
+        }
+
+        Canvas FindWorldCanvas()
+        {
+            foreach (Canvas c in FindObjectsOfType<Canvas>())
+            {
+                if (c.renderMode == RenderMode.WorldSpace)
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+    }
+}
+
