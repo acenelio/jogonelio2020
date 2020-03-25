91768e71afcc1fe25ddbd1fa65b44704eae5b6db
Nelio Alves <acenelio@gmail.com>
2020-03-25 17:42:32 -0300

    Saving enemies in aggro range

---
+++ Assets/Scripts/NavGame/Extensions/UnityExtensions.cs
@@ -0,0 +1,11 @@
+﻿// https://answers.unity.com/questions/50279/check-if-layer-is-in-layermask.html
+namespace UnityEngine
+{
+    public static class UnityExtensions
+    {
+        public static bool Contains(this LayerMask mask, int layer)
+        {
+            return mask == (mask | (1 << layer));
+        }
+    }
+}
