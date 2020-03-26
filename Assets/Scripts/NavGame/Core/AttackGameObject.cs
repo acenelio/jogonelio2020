8db558adac914039cf5c21c396ecc45c02cb6db6
Nelio Alves <acenelio@gmail.com>
2020-03-25 23:17:31 -0300

    Damageable and Attack independent classes

---
+++ Assets/Scripts/NavGame/Core/AttackGameObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NavGame.Managers;
+
+namespace NavGame.Core
+{
+    public class AttackGameObject : TouchableGameObject
+    {
+        public OfenseStats ofenseStats;
+        float cooldown = 0f;
+
+        public OnAttackHitEvent onAttackHit;
+
+        protected virtual void Update()
+        {
+            DecreaseAttackCooldown();
+        }
+
+        public void AttackOnCooldown(DamageableGameObject target)
+        {
+            if (cooldown <= 0f)
+            {
+                cooldown = 1f / ofenseStats.attackSpeed;
+                target.TakeDamage(ofenseStats.damage);
+                if (onAttackHit != null)
+                {
+                    onAttackHit(target.transform.position);
+                }
+            }
+        }
+
+        void DecreaseAttackCooldown()
+        {
+            if (cooldown == 0f)
+            {
+                return;
+            }
+            cooldown -= Time.deltaTime;
+            if (cooldown < 0f)
+            {
+                cooldown = 0f;
+            }
+        }
+    }
+}
