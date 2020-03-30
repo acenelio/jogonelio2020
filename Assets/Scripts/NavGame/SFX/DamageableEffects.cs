1e0654bdcb394ecf6f4b9359eccecaaf3e951038
Nelio Alves <acenelio@gmail.com>
2020-03-30 00:20:40 -0300

    SFXController, Damageable effects

---
+++ Assets/Scripts/NavGame/SFX/DamageableEffects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NavGame.Core;
+using NavGame.Managers;
+
+namespace NavGame.Effects
+{
+    [RequireComponent(typeof(DamageableGameObject))]
+    public class DamageableEffects : SFXController
+    {
+        public string damageSound;
+        public string dieSound;
+
+        public GameObject damageEffects;
+        public GameObject dieEffects;
+
+        protected virtual void Awake()
+        {
+            DamageableGameObject damageable = GetComponent<DamageableGameObject>();
+            damageable.onDamageTaken += OnDamageTaken;
+            damageable.onDied += OnDied;
+        }
+
+        protected virtual void OnDamageTaken(Vector3 point, int amount)
+        {
+            PlayEffects(point, damageSound, damageEffects);
+        }
+
+        protected virtual void OnDied()
+        {
+            PlayEffects(transform.position, dieSound, dieEffects);
+        }
+    }
+}
---
+++ Assets/Scripts/NavGame/SFX/SFXController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NavGame.Core;
+using NavGame.Managers;
+
+namespace NavGame.Effects
+{
+    public abstract class SFXController : MonoBehaviour
+    {
+        protected void PlayEffects(Vector3 point, string sound, GameObject effects)
+        {
+            AudioManager.instance.Play(sound, point);
+            if (effects != null)
+            {
+                Instantiate(effects, point, Quaternion.identity);
+            }
+        }
+    }
+}
