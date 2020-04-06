fab6cb3de8bbed0a31705fa0823ed0746606e55e
Nelio Alves <acenelio@gmail.com>
2020-04-06 17:47:36 -0300

    Damageable damage taken event

--- Assets/Scripts/NavGame/Core/Events.cs
+++ Assets/Scripts/NavGame/Core/Events.cs
@@ -4,6 +4,7 @@ namespace NavGame.Core
 {
     // CHARACTER EVENTS
     public delegate void OnAttackHitEvent(Vector3 position);
+    public delegate void OnDamageTakenEvent(Vector3 strikePoint, int amount);
     public delegate void OnHealthChangedEvent(int maxHealth, int currentHealth);
     public delegate void OnDiedEvent();
 }
