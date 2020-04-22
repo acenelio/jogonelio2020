3e33a3664cda78d56a7c85d9ba6999ff888bfc61
Nelio Alves <acenelio@gmail.com>
2020-04-22 17:02:55 -0300

    ThrowerGameObject

---
+++ Assets/Scripts/NavGame/Core/ThrowerGameObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core
+{
+    public class ThrowerGameObject : AttackGameObject
+    {
+        public GameObject projectilePrefab;
+
+        protected override void Attack(DamageableGameObject target)
+        {
+            InstantiateProjectile(target);
+        }
+
+        void InstantiateProjectile(DamageableGameObject target)
+        {
+            GameObject projectile = Instantiate(projectilePrefab, castTransform.position, Quaternion.identity) as GameObject;
+            ProjectileController controller = projectile.GetComponent<ProjectileController>();
+            controller.Init(target, ofenseStats.damage);
+            controller.onAttackStrike += OnAttackStrike;
+        }
+
+        void OnAttackStrike(Vector3 strikePoint)
+        {
+            if (onAttackStrike != null)
+            {
+                onAttackStrike(strikePoint);
+            }
+        }
+    }
+}
