d618a079fce077546dc5c3e79aa03ee4867a9077
Nelio Alves <acenelio@gmail.com>
2020-04-23 00:09:47 -0300

    ProjectileController part 1

---
+++ Assets/Scripts/NavGame/Core/ProjectileController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core
+{
+    public class ProjectileController : MonoBehaviour
+    {
+        public float speed = 8f;
+
+        DamageableGameObject target;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if (target == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, target.damageTransform.position, speed * Time.deltaTime);
+        }
+
+        public void Init(DamageableGameObject target)
+        {
+            this.target = target;
+        }
+    }
+}
