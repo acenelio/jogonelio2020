b07653e7f66683060114e641cb2ab061b3370101
Nelio Alves <acenelio@gmail.com>
2020-05-02 22:47:45 -0300

    UIManager

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject[] actionCDPanels;
+
+    Image[] actionCDImages;
+
+    void Start()
+    {
+        InitializeUI();        
+    }
+
+    void InitializeUI()
+    {
+        actionCDImages = new Image[actionCDPanels.Length];
+        for (int i = 0; i < actionCDPanels.Length; i++)
+        {
+            actionCDImages[i] = actionCDPanels[i].GetComponent<Image>();
+            actionCDImages[i].fillAmount = 0f;
+        }
+    }
+}
