55e5ec12bf33453a025c166145e9835106462fc9
Nelio Alves <acenelio@gmail.com>
2020-05-03 18:10:06 -0300

    Performing action

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using NavGame.Core;
+using NavGame.Managers;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : TouchableGameObject
@@ -36,6 +37,9 @@ public class PlayerController : TouchableGameObject
     {
         if (Input.GetMouseButtonDown(1))
         {
+            LevelManager.instance.CancelAction();
+            actionPoint = Vector3.zero;
+
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
@@ -54,7 +58,7 @@ public class PlayerController : TouchableGameObject
                 pickupTarget = null;
             }
         }
-        else if (Input.GetMouseButtonDown(0))
+        else if (Input.GetMouseButtonDown(0) && LevelManager.instance.IsActionSelected())
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -64,6 +68,19 @@ public class PlayerController : TouchableGameObject
                 actionPoint = hit.point;
                 agent.SetDestination(hit.point);
             }            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            LevelManager.instance.SelectAction(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            LevelManager.instance.SelectAction(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            LevelManager.instance.SelectAction(2);
         }
     }
 
@@ -85,6 +102,7 @@ public class PlayerController : TouchableGameObject
             if (Vector3.Distance(transform.position, actionPoint) <= range)
             {
                 agent.ResetPath();
+                LevelManager.instance.DoAction(actionPoint);
                 actionPoint = Vector3.zero;
             }
         }
--- Assets/Scripts/NavGame/Managers/LevelManager.cs
+++ Assets/Scripts/NavGame/Managers/LevelManager.cs
@@ -7,11 +7,52 @@ namespace NavGame.Managers
 {
     public abstract class LevelManager : MonoBehaviour
     {
+        public static LevelManager instance;
+
         public Action[] actions;
+
+        protected int selectedAction = -1;
+
+        protected virtual void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else 
+            {
+                Destroy(gameObject);
+            }
+        }
 
         void Start()
         {
             StartCoroutine(SpawnBad());
+        }
+
+        public virtual void SelectAction(int actionIndex)
+        {
+            Debug.Log("Selected: " + actions[actionIndex].prefab.name);
+            selectedAction = actionIndex;
+        }
+
+        public virtual void DoAction(Vector3 point)
+        {
+            Debug.Log("Do: " + actions[selectedAction].prefab.name);
+            Instantiate(actions[selectedAction].prefab, point, Quaternion.identity);
+        }
+
+        public virtual void CancelAction()
+        {
+            if (selectedAction != -1)
+            {
+                selectedAction = -1;
+            }
+        }
+
+        public bool IsActionSelected()
+        {
+            return selectedAction != -1;
         }
 
         protected abstract IEnumerator SpawnBad();
