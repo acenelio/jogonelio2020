f4407fe73f49206fb41687b2e543211498679842
Nelio Alves <acenelio@gmail.com>
2020-05-03 17:45:34 -0300

    Action, abstract LevelManager

---
+++ Assets/Scripts/NavGame/Managers/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Managers
+{
+    public abstract class LevelManager : MonoBehaviour
+    {
+        public Action[] actions;
+
+        void Start()
+        {
+            StartCoroutine(SpawnBad());
+        }
+
+        protected abstract IEnumerator SpawnBad();
+
+        [Serializable]
+        public class Action
+        {
+            public int cost;
+            public GameObject prefab;
+            public float waitTime = 1f;
+            public float coolDown;
+        }
+    }
+}
