96ccfe6d363ea899379daf039e33006d7bb64296
Nelio Alves <acenelio@gmail.com>
2020-05-03 16:59:24 -0300

    Action move left mouse button

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -9,10 +9,15 @@ public class PlayerController : TouchableGameObject
 {
     NavMeshAgent agent;
     Camera cam;
+
+    public float range = 4f;
+
     public LayerMask walkableLayer;
     public LayerMask collectibleLayer;
 
     CollectibleGameObject pickupTarget;
+
+    Vector3 actionPoint = Vector3.zero;
 
     void Awake()
     {
@@ -24,6 +29,7 @@ public class PlayerController : TouchableGameObject
     {
         ProcessInput();
         UpdateCollect();
+        UpdateAction();
     }
 
     void ProcessInput()
@@ -48,6 +54,17 @@ public class PlayerController : TouchableGameObject
                 pickupTarget = null;
             }
         }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, walkableLayer))
+            {
+                actionPoint = hit.point;
+                agent.SetDestination(hit.point);
+            }            
+        }
     }
 
     void UpdateCollect()
@@ -57,6 +74,18 @@ public class PlayerController : TouchableGameObject
             if (IsInTouch(pickupTarget))
             {
                 pickupTarget.Pickup();
+            }
+        }
+    }
+
+    void UpdateAction()
+    {
+        if (actionPoint != Vector3.zero)
+        {
+            if (Vector3.Distance(transform.position, actionPoint) <= range)
+            {
+                agent.ResetPath();
+                actionPoint = Vector3.zero;
             }
         }
     }
