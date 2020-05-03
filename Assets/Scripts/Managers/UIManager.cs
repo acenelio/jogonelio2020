8918a0845a9c7c4adde9dbd9fb8252c4d2b8618f
Nelio Alves <acenelio@gmail.com>
2020-05-03 19:09:18 -0300

    UIManager

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject[] cooldownObjects;
+
+    Image[] cooldownImages;
+
+    void Start()
+    {
+        InitializeUI();
+    }
+
+    void InitializeUI()
+    {
+        cooldownImages = new Image[cooldownObjects.Length];
+        for (int i = 0; i < cooldownObjects.Length; i++)
+        {
+            cooldownImages[i] = cooldownObjects[i].GetComponent<Image>();
+            cooldownImages[i].fillAmount = 0f;
+        }
+    }
+}
