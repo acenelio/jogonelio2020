dfe257163b4ceb662718e09be2e5bce5ccbe8b42
Nelio Alves <acenelio@gmail.com>
2020-05-08 14:16:38 -0300

    LevelData

---
+++ Assets/Scripts/NavGame/Models/LevelData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NavGame.Models
+{
+    public class LevelData
+    {
+        public int CoinCount { get; private set; }
+
+        public void AddCoins(int amount)
+        {
+            CoinCount += amount;
+        }
+
+        public void ConsumeCoins(int amount)
+        {
+            ValidateCoinAmount(amount);
+            CoinCount -= amount;
+        }
+
+        public void ValidateCoinAmount(int amount)
+        {
+            if (CoinCount < amount)
+            {
+                throw new InvalidOperationException("Need " + amount + " coins!");
+            }
+        }
+    }
+}
