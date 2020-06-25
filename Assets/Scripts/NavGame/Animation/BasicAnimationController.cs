a7c89275a053597390e167ac652d87ab8407bf16
Nelio Alves <acenelio@gmail.com>
2020-06-24 21:59:05 -0300

    Raposita Animation Controller

---
+++ Assets/Scripts/NavGame/Animation/BasicAnimationController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace NavGame.Animation
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class BasicAnimationController : MonoBehaviour
+    {
+        public float smoothness = 0.1f;
+
+        protected NavMeshAgent agent;
+        protected Animator animator;
+
+        protected virtual void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        protected virtual void Update()
+        {
+            float speedPercent = agent.velocity.magnitude / agent.speed;
+            animator.SetFloat("speedPercent", speedPercent, smoothness, Time.deltaTime);
+        }
+    }
+}
